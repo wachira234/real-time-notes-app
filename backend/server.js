732ad37089516eb7ce43a1f6701479b254a0e5e1
wachira234 <wachiramboche2@gmail.com>
2025-03-22 23:10:13 +0300

    netlifylinkupdate

--- backend/server.js
+++ backend/server.js
@@ -11,7 +11,7 @@ const server = http.createServer(app);
 
 const io = socketIo(server, {
   cors: {
-    origin: "http://localhost:5173",
+    origin: "https://notes-app-mongodb.netlify.app/",
     methods: ["GET", "POST"]
   }
 });
--- frontend/src/socket.jsx
+++ frontend/src/socket.jsx
@@ -1,2 +1,2 @@
 import { io } from 'socket.io-client';
-export const socket = io('https://real-time-notes-backend-6qdj.onrender.com', { autoConnect: false });
+export const socket = io('https://real-time-notes-app.onrender.com', { autoConnect: false });
