b40ebaf9206b49c956e155c9f9e92e24e864e2ae
wachira234 <wachiramboche2@gmail.com>
2025-03-23 00:39:07 +0300

    Export UserContext to fix Netlify build error

--- frontend/src/contexts/UserContext.jsx
+++ frontend/src/contexts/UserContext.jsx
@@ -1,12 +1,8 @@
 import React, { createContext, useState } from 'react';
 
-const UserContext = createContext();
+export const UserContext = createContext();
 
 export const UserProvider = ({ children }) => {
   const [username] = useState('User' + Math.floor(Math.random() * 1000));
   return <UserContext.Provider value={{ username }}>{children}</UserContext.Provider>;
 };
-
-export const useUser = () => useContext(UserContext);
-
-export default UserContext;
