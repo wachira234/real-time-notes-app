c25585c09af2264b55765461df84879e4258507c
wachira234 <wachiramboche2@gmail.com>
2025-03-22 22:55:13 +0300

    Update App.jsx

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,5 +1,5 @@
 import { BrowserRouter as Router, Routes, Route, useNavigate } from 'react-router-dom';
-import NoteEditor from './NoteEditor';
+import NoteEditor from './NoteEditor.jsx';
 
 function Home() {
   const navigate = useNavigate();
