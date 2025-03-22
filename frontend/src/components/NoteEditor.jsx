c9223d1e4fd7f37169405b11d2be03736100f08c
wachira234 <wachiramboche2@gmail.com>
2025-03-22 23:14:27 +0300

    Update NoteEditor.jsx

--- frontend/src/components/NoteEditor.jsx
+++ frontend/src/components/NoteEditor.jsx
@@ -12,7 +12,7 @@ const NoteEditor = () => {
 
   useEffect(() => {
     // Fetch initial note content
-    fetch(`https://real-time-notes-backend-6qdj.onrender.com/api/notes/${id}`)
+    fetch(`https://real-time-notes-app.onrender.com/api/notes/${id}`)
       .then((res) => res.json())
       .then((data) => setContent(data.content))
       .catch((err) => console.error('Error fetching note:', err));
